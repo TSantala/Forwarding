dedda1c79c2583e8f825b198e2cf7e30bc07a3ae
TSantala <timo.santala@duke.edu>
2015-03-18 21:34:22 -0400

    Working on clock skew problem (probably); pushing to debug separately

--- buffer4.vhd
+++ buffer4.vhd
@@ -8,17 +8,20 @@ entity buffer4 is
 	port (
 		clk, enable, reset: in std_logic;
 		portOne,portTwo, portThree, portFour: in std_logic_vector (7 downto 0); -- one byte at time for all inputs
-		lngth_in: in std_logic_vector (15 downto 0); -- know how long to send data before switching ports...think about this
+		lngth_one, lngth_two, lngth_three, lngth_four: in std_logic_vector (15 downto 0); -- know how long to send data before switching ports...think about this
 		output: out std_logic_vector (7 downto 0); -- out one byte at a time
+		state_debug: out std_logic_vector(2 downto 0);
 		portNum: out std_logic_vector(2 downto 0) -- port number to send to table
 		); 
 end buffer4;
+
+-- OUTPUT_VALID to flag when we're in transition or not?
 
 architecture selector of buffer4 is 
 	type state_type is
 		(A, B, C, D, E);
 	signal state_reg, state_next: state_type;
-	signal current_length : std_logic_vector(15 downto 0);
+	signal current_length : unsigned(15 downto 0);
 begin
 	process (clk, enable, reset) --state register update
 	begin
@@ -28,11 +31,13 @@ begin
 		end if;
 	end process;
 	
-	process(state_reg, enable, lngth_in, portOne, portTwo, portThree, portFour)
+	process(state_reg, enable, current_length, lngth_one, lngth_two, lngth_three, lngth_four, portOne, portTwo, portThree, portFour)
 	begin
 		case state_reg is
 		when A => -- reset state, wait for enable
 			portNum <= "000";
+			state_debug <= "000";
+			output <= "00000000";
 			if (enable = '1') then
 				state_next <= B;
 			else
@@ -40,9 +45,10 @@ begin
 			end if;
 		when B => --port one
 			portNum <= "001";
+			state_debug <= "001";
 			output <= portOne;
-			current_length <= std_logic_vector( unsigned(current_length) + 1 );
-			if (current_length = lngth_in) then
+			current_length <= ( unsigned(current_length) + 1 );
+			if (current_length = unsigned(lngth_one) + 18) then		-- +18 for non-data overhead
 				state_next <= C;
 				current_length <= "0000000000000000";
 			else
@@ -50,9 +56,10 @@ begin
 			end if;
 		when C => --port two
 			portNum <= "010";
+			state_debug <= "010";
 			output <= portTwo;
-			current_length <= std_logic_vector( unsigned(current_length) + 1 );
-			if (current_length = lngth_in) then
+			current_length <= ( unsigned(current_length) + 1 );
+			if (current_length = unsigned(lngth_two) + 18) then
 				state_next <= D;
 				current_length <= "0000000000000000";
 			else
@@ -60,9 +67,10 @@ begin
 			end if;
 		when D => --port three
 			portNum <= "011";
+			state_debug <= "011";
 			output <= portThree;
-			current_length <= std_logic_vector( unsigned(current_length) + 1 );
-			if (current_length = lngth_in) then
+			current_length <= ( unsigned(current_length) + 1 );
+			if (current_length = unsigned(lngth_three) + 18) then
 				state_next <= E;
 				current_length <= "0000000000000000";
 			else
@@ -70,9 +78,10 @@ begin
 			end if;
 		when E => --port four
 			portNum <= "100";
+			state_debug <= "100";
 			output <= portFour;
-			current_length <= std_logic_vector( unsigned(current_length) + 1 );
-			if (current_length = lngth_in) then
+			current_length <= ( unsigned(current_length) + 1 );
+			if (current_length = unsigned(lngth_four) + 18) then
 				state_next <= B;
 				current_length <= "0000000000000000";
 			else
